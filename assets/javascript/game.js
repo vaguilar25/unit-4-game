9cd9fe0676fe6cffbc44e0ab936f41d1388d7178
vaguilar25 <vivian.aguilar@gmail.com>
2018-11-12 03:00:44 -0600

    adding object for the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,40 @@
 $(function () {
+   
+    $("#attackButton").prop("disabled",true);
+    function gameCharacter(nameChar, attPow, countAttPow,hPoints) {
+        this.name= nameChar;
+        this.attackPower=attPow;
+        this.counterAttackPower=countAttPow;
+        this.healthPoints=hPoints;
+    };
+
+    var obiwanCharacter= new gameCharacter("obiwan",8,10,120);
+    var lukeCharacter= new gameCharacter("luke",2,5,100);
+    var sidiousCharacter= new gameCharacter("sidious",10,20,150);
+    var maulCharacter= new gameCharacter("maul",15,25,180);
+    
+    $(document).on("click", "#attackButton", function (event) {
+       //var currentCharacter = $(".imageChar").data();
+       var currentCharacter = $(".imageChar").attr("data");
+       var currentDefender = $(".imageDefender").attr("data");
+
+
+       console.log(currentCharacter + " vs " + currentDefender);
+    });
+
+    //Function healthPoints Character decrease
+    function healthPointsDecrease(nameChar) {
+        
+    }
+
+    //Function healthPoints Defender Decrease
+
+    //Function check winner return defender Win or Character Win.. 
+    //Global variable of points take the decrease value of Character Win
+
+
+
+
     //Function to remove a class from an array
     function removeClass(arrayElements, className) {
         arrayElements.forEach(function (element) {
@@ -28,14 +64,15 @@ $(function () {
     };
 
     function moveEnemiToDefender(enemi) {
-       $(enemi).removeClass("imageEnemi");
-       $(enemi).addClass("imageDefender");
+        $(enemi).removeClass("imageEnemi");
+        $(enemi).addClass("imageDefender");
     };
 
     //Get OnClick listener for the images
+
     $(document).on("click", ".imageChar", function (event) {
         //Move the images that are not selected to enemies
-
+     //   $("#attackButton").click(function () { return false; });
 
         switch ($(this).attr("data")) {
             case "obiwan":
@@ -67,21 +104,32 @@ $(function () {
 
 
     });
-
+    //disable click on Enemies
+    function disableClickOnEnemi(enemiesLeft) {
+        enemiesLeft.forEach(function (element) {
+            $(element).click(function () { return false; });
+        });
+    };
     $(document).on("click", ".imageEnemi", function (event) {
+        
         switch ($(this).attr("data")) {
+            
             case "obiwan":
                 //move Characters to Enemies and change the class
                 $("#obi").appendTo("#defender");
                 moveEnemiToDefender("#obi");
-                ("#luk").off(click);
+                //var enemiesLeft=["luk","sid"]; 
+                //disableClickOnEnemi(enemiesLeft);
+                $("#enemies").click(function () { return false; });
                 break;
 
             case "luke":
                 //move Characters to Enemies and change the class
                 $("#luk").appendTo("#defender");
                 moveEnemiToDefender("#luk");
-                ("#sid").off(click);
+
+                //var enemiesLeft=["","sid"]; 
+                //disableClickOnEnemi(enemiesLeft);
                 break;
             case "sidious":
 
@@ -96,8 +144,14 @@ $(function () {
                 moveEnemiToDefender("#mau");
                 break;
         }
+        //disable click on enemies
+        $("#enemies").click(function () { return false; });
 
-        });
-    
+        //enable attack
+        //$("#attackButton").click(function () { return true; });
+        $("#attackButton").prop("disabled",false);
+
+    });
+
 
 })
